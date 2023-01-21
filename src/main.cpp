36c8b0a834b8e1c9fbe2e30883fe8c73dbf0806a
qadrillah <qadrillah@mhs.unsyiah.ac.id>
2023-01-21 20:55:22 +0700

    merge program suhu and PH

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,73 @@
 #include <Arduino.h>
 
-#define PIN_SENSOR_PH A1
-// bagian ini yang perlu diatur
-#define PH4 3.1 // berapa tegangan ketika ph 4?
-#define PH7 2.6 // berapa tergangan ketika ph 7?
+#include <DallasTemperature.h>
+#include <OneWire.h>
 
+// pin data sensor yang dihubungkan pada pin Analog
+#define PIN_SENSOR_SUHU A0
+#define PIN_SENSOR_PH A1
+
+// Setup oneWire instance to communicate with any OneWire devices
+OneWire oneWire(PIN_SENSOR_SUHU);
+DallasTemperature suhu(&oneWire);
+
+// bagian ini yang perlu diatur jika ingin kalibrasi
+/*
+jika ingin kalibrasi PH ubah bool kalibrasi = true;
+jika tidak kalibrasi ubah    bool kalibrasi = false;
+*/
+bool kalibrasi = false;
+#define PH4 3.0 // berapa tegangan ketika ph 4? 3.0
+#define PH7 2.6 // berapa tergangan ketika ph 7? 2.6
+
+// setup program => hanya akan berjalan sekali saja ketika baru dijalankan
 void setup()
 {
-  // initialize digital pin LED_BUILTIN as an output.
+  // insisiasi PIN_SENSOR_PH sebagai input
   pinMode(PIN_SENSOR_PH, INPUT);
+  // inisiasi serial monitor untuk melihat output program
   Serial.begin(9600);
+  // inisiasi sensor suhu
+  suhu.begin();
 }
 
-// the loop function runs over and over again forever
+// program yang akan berjalan selalu
 void loop()
 {
-  float phValue = analogRead(PIN_SENSOR_PH) ;
-  float voltage = phValue * (5.0 / 1023.0);
-  float phStep  = (PH4 - PH7) / 3;
-  float pO      = 7.00 + ((PH7 - voltage) / phStep);
+  if (!kalibrasi){
+    // baca sensor PH
+    float phValue = analogRead(PIN_SENSOR_PH);        // baca nilai pin A0
+    float voltage = phValue * (5.0 / 1023.0);         // ubah nilainya menjadi tegangan
+    float phStep  = (PH4 - PH7) / 3;                  // dapatkan nilai phStep => (PH4 - PH7) / (7 - 4)
+    float pO      = 7.00 + ((PH7 - voltage) / phStep);// dapatkan nilai PH asli
 
-  Serial.print("Voltage : ");
-  Serial.print(voltage);
-  Serial.print(" -- PH : ");
-  Serial.println(pO);
+    // baca sensor suhu
+    suhu.requestTemperatures();
+    // tampilkan nilai sensor suhu
+    Serial.print("Suhu : ");
+    Serial.print(suhu.getTempCByIndex(00));
+    Serial.print(" C");
 
-  delay(500);
+    // tampilkan nilai sensor PH
+    Serial.print(" | PH : ");
+    Serial.println(pO);
+
+    // jeda program selama satu detik
+    delay(1000);
+  }else{
+    // baca sensor PH
+    float phValue = analogRead(PIN_SENSOR_PH);
+    float voltage = phValue * (5.0 / 1023.0);
+    float phStep = (PH4 - PH7) / 3;
+    float pO = 7.00 + ((PH7 - voltage) / phStep);
+
+    // tampilkan nilai sensor PH
+    Serial.print("Tegangan : ");
+    Serial.print(voltage);
+    Serial.print(" -- PH : ");
+    Serial.println(pO);
+
+    // jeda program selama setengah detik
+    delay(500);
+  }
 }
